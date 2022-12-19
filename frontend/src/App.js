e1ac9caa025046bff6761aa183f0c51eb7e55977
mahima <076bct033.mahima@pcampus.edu.np>
2022-12-19 23:00:59 +0545

    data sucessfullt fetched from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,48 @@
-import './App.css';
 import React, { useState, useEffect } from "react";
-function App() {
+import "./App.css";
+import Table from "@mui/material/Table";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Data } from "./Data";
 
+export default function BasicTable() {
   const [data, setData] = useState();
 
-useEffect(() => {
-  fetch("http://127.0.0.1:8000/student/")
-  .then((response) => response.json())
-  .then((response) => console.log(response));
-
-}, []);
-  const  onClick = () => {
-    console.log("Button clicked");
-    }
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/student/")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+      });
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-       <button onClick={onClick()}>Data</button>
-      </header>
-    </div>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="center">Roll No</TableCell>
+              <TableCell align="center">City</TableCell>
+            </TableRow>
+
+            {data?.map((event) => {
+              return (
+                <Data
+                  key={event.name}
+                  name={event.name}
+                  roll={event.roll}
+                  city={event.city}
+                />
+              );
+            })}
+          </TableHead>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
-
-export default App;
