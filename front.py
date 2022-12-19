001af762708711f2355fa555b3b5c6e299f3f3f3
mahima <076bct033.mahima@pcampus.edu.np>
2022-12-19 23:07:12 +0545

    requests working

--- front.py
+++ front.py
@@ -1,3 +1,4 @@
+#Fetching data from the server
 import requests
 url='http://127.0.0.1:8000/student/'
 data=requests.get(url)
--- frontend/src/Data.js
+++ frontend/src/Data.js
@@ -2,11 +2,9 @@ import React from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 export const Data = (props) => {
-  // props are type of  object where value of attributes of a tag is stored
   return (
     <TableRow>
-      {/* <TableCell>{props.id}</TableCell> */}
-      <TableCell align="center">{props.name}</TableCell>
+          <TableCell align="center">{props.name}</TableCell>
       <TableCell align="center">{props.roll}</TableCell>
       <TableCell align="center">{props.city}</TableCell>
     
